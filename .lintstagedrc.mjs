48a18bd49c12fab45ec7dd1ef0d2a6b2d0eb2583
Temiloluwa Alabi <temiloluwaalabi33@gmail.com>
2026-02-16 14:11:43 +0100

    fix: husky hooks corrected

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,29 +1,7 @@
-// const LintStagedConfig = {
-//   // Target everything EXCEPT the shadcn ui folder
-//   '!(components/ui/**)*.{js,jsx,ts,tsx}': ['eslint --fix', 'prettier --write'],
-//   // Format everything else
-//   '*.{json,css,md}': ['prettier --write'],
-// };
-
-// export default LintStagedConfig;
-
 // .lintstagedrc.mjs
-import path from 'path';
-
-const lint = {
-  '*.{js,jsx,ts,tsx}': (filenames) => {
-    const filteredFiles = filenames
-      .filter((file) => !file.includes(path.join('components', 'ui')))
-      .map((f) => `"${path.relative(process.cwd(), f)}"`);
-
-    if (filteredFiles.length === 0) return [];
-
-    return [
-      `eslint --fix ${filteredFiles.join(' ')}`,
-      `prettier --write ${filteredFiles.join(' ')}`,
-    ];
-  },
+export default {
+  // Check and fix all JS/TS files
+  '*.{js,jsx,ts,tsx}': ['eslint --fix', 'prettier --write'],
+  // Format only for other file types
   '*.{json,css,md}': ['prettier --write'],
 };
-
-export default lint;
